319d5aabef933956b177f132767c6c44a9ddcb8a
mmirzafahlefi <nyobatyuk0910@gmail.com>
2026-06-10 18:45:26 +0700

    Make Echo/Pusher initialization conditional to avoid JS errors

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -11,10 +11,12 @@ import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-    forceTLS: true,
-    authEndpoint: '/api/broadcasting/auth',
-});
+if (import.meta.env.VITE_PUSHER_APP_KEY) {
+    window.Echo = new Echo({
+        broadcaster: 'pusher',
+        key: import.meta.env.VITE_PUSHER_APP_KEY,
+        cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
+        forceTLS: true,
+        authEndpoint: '/api/broadcasting/auth',
+    });
+}
