a978c2d90fea7a2a5d391bd24b121893499a9b5f
mmirzafahlefi <nyobatyuk0910@gmail.com>
2026-06-10 18:54:39 +0700

    Exclude webauthn routes from CSRF protection

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,10 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->trustProxies(at: '*');
+
+        $middleware->validateCsrfTokens(except: [
+            'webauthn/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
